fffe047147886102493952869c0242233d8284e8
Jason Founts <jason@jasonfounts.com>
2015-06-30 11:12:45 -0700

    Updated home page and main.js to target and adjust featured content only

--- source/js/main.js
+++ source/js/main.js
@@ -14,15 +14,15 @@ function adjustHeadingHeight(){
   //width check //748.8px bp-med
   if ($("body").width() > 748.8) {
     iterate();
-    $(".gi .b-title").height(h);
+    $("#featured-content .gi .b-title").height(h);
   }else {
-    $(".gi .b-title").each(function(){
-      $(".gi .b-title").css("height", "");
+    $("#featured-content .gi .b-title").each(function(){
+      $("#featured-content .gi .b-title").css("height", "");
     });
   }
   //find the tallest heading by iterating through them all
   function iterate(){
-    $(".gi .b-title").each(function(index){
+    $("#featured-content .gi .b-title").each(function(index){
       //if h is empty, set it
       //else compare with current item, if bigger set it, else do nothing
       if(h == null) {
